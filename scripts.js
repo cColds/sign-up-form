e4ae18b4e98b9357d63e914897e488c853d459af
Thaddaeus Fam <thaddaeusfamous@gmail.com>
2022-08-26 07:52:29 -0500

    fix email

--- scripts.js
+++ scripts.js
@@ -39,7 +39,8 @@ function isPasswordValid(password, confirmPassword) {
 		specialCharInfo.textContent = "✖At least one special character";
 	}
 	if (allRequirementsMet()) {
-		submitButton.disabled = false;
+		if (password.value === confirmPassword.value)
+			submitButton.disabled = false;
 	} else {
 		submitButton.disabled = true;
 	}
@@ -74,7 +75,7 @@ function allRequirementsMet() {
 		if (digitsInfo.style.color == "green") {
 			if (upperCaseInfo.style.color == "green") {
 				if (specialCharInfo.style.color == "green") {
-					if (password.value === confirmPassword.value) return true;
+					return true;
 				}
 			}
 		}
@@ -123,10 +124,6 @@ function isUpperCase(password) {
 		}
 	}
 	return false;
-}
-
-function invalidUpperCase(password) {
-	console.log(password.textContent);
 }
 
 function includesSpecialCharacters(password) {
@@ -202,16 +199,24 @@ email.addEventListener("keyup", () => styleEmail(email.value));
 
 function styleEmail(input) {
 	const alphanumerical = /[^\W]|_/g;
-	if (input[input.length - 2] == "@") {
-		if (
-			input[input.length - 1].match(alphanumerical) ==
-			input[input.length - 1]
-		) {
+	if (input.includes("@")) {
+		const atSymbol = input.indexOf("@");
+		const atSymbolLength = atSymbol + 1;
+		if (input[atSymbolLength]) {
+			if (input[atSymbolLength].match(alphanumerical)) {
+				email.style.borderStyle = "solid";
+				email.style.borderColor = "#596d48";
+				email.style.backgroundColor = "white";
+				emailSymbol.style.color = "green";
+				emailSymbol.textContent = "✓";
+			}
+		} else {
 			email.style.borderStyle = "solid";
-			email.style.borderColor = "#596d48";
-			email.style.backgroundColor = "white";
-			emailSymbol.style.color = "green";
-			emailSymbol.textContent = "✓";
+			email.style.borderColor = "#e5e7eb";
+			email.style.backgroundColor = " #FF7276";
+			emailSymbol.style.color = "#FF033E";
+
+			emailSymbol.textContent = "✖";
 		}
 	} else {
 		email.style.borderStyle = "solid";
@@ -220,5 +225,57 @@ function styleEmail(input) {
 		emailSymbol.style.color = "#FF033E";
 
 		emailSymbol.textContent = "✖";
+	}
+}
+
+const passwordInput = document.querySelector("#passwordId");
+const passwordSymbol = document.querySelector(".passwordSymbol");
+passwordSymbol.style.color = "transparent";
+passwordInput.addEventListener("keyup", () =>
+	stylePassword(passwordInput.value)
+);
+
+function stylePassword() {
+	if (allRequirementsMet()) {
+		{
+			passwordInput.style.borderStyle = "solid";
+			passwordInput.style.borderColor = "#596d48";
+			passwordInput.style.backgroundColor = "white";
+			passwordSymbol.style.color = "green";
+			passwordSymbol.textContent = "✓";
+		}
+	} else {
+		passwordInput.style.borderStyle = "solid";
+		passwordInput.style.borderColor = "#e5e7eb";
+		passwordInput.style.backgroundColor = " #FF7276";
+		passwordSymbol.style.color = "#FF033E";
+
+		passwordSymbol.textContent = "✖";
+	}
+}
+
+const confirmPasswordInput = document.querySelector("#confirm-password");
+const confirmPasswordSymbol = document.querySelector(".confirmPasswordSymbol");
+confirmPasswordSymbol.style.color = "transparent";
+confirmPasswordInput.addEventListener("keyup", () =>
+	styleConfirmPassword(confirmPassword.value)
+);
+
+function styleConfirmPassword() {
+	if (allRequirementsMet()) {
+		{
+			confirmPasswordInput.style.borderStyle = "solid";
+			confirmPasswordInput.style.borderColor = "#596d48";
+			confirmPasswordInput.style.backgroundColor = "white";
+			confirmPasswordSymbol.style.color = "green";
+			confirmPasswordSymbol.textContent = "✓";
+		}
+	} else {
+		confirmPasswordInput.style.borderStyle = "solid";
+		confirmPasswordInput.style.borderColor = "#e5e7eb";
+		confirmPasswordInput.style.backgroundColor = "#FF7276";
+		confirmPasswordSymbol.style.color = "#FF033E";
+
+		confirmPasswordSymbol.textContent = "✖";
 	}
 }
